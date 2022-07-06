51ad39872c9bc33948c5f5d44520bea46d090a28
Connfeeg <confeeg@gmail.com>
2022-07-06 14:17:14 +0400

    0

--- src/admin/app.js
+++ src/admin/app.js
@@ -3,7 +3,7 @@ export default {
     locales: ['ru'],
     translations: {
       ru: {
-        'app.components.LeftMenu.navbrand.title': 'Тест2',
+        'app.components.LeftMenu.navbrand.title': 'Тест',
         'app.components.HomePage.welcome': 'Тестовый проект',
       },
     }
